d6c5adeebac215a3d104e44b395590ac186ec767
Michele Nuzzi <michele.nuzzi.2014@gmail.com>
2024-05-14 22:37:02 +0200

    ToDataVersion

--- src/toData/interface.ts
+++ src/toData/interface.ts
@@ -1,5 +1,7 @@
 import { Data } from "../Data";
 
+export type ToDataVersion = "v1" | "v2" | "v3";
+
 export interface ToData {
-    toData: ( version?: "v1" | "v2" | "v3" ) => Data
+    toData: ( version?: ToDataVersion ) => Data
 }
